77708e7a6cdde24e2d3075fa9d27988359c23aae
ikezaki <ikezaki@social-db.co.jp>
2025-12-10 23:55:45 +0900

    Add: `vue-sonner`のスタイルシートをインポート

--- src/entrypoints/editor/main.ts
+++ src/entrypoints/editor/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './style.css';
 import VueKonva from 'vue-konva';
+import 'vue-sonner/style.css';
 
 createApp(App).use(VueKonva).mount('#app');
